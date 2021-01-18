975107fb807c7d9d3519d023ea33eba9fd9c9280
Angel_Brown <arbrown110@gmail.com>
2021-01-17 22:42:33 -0500

    index and app.js mofifications.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import Main from './components/Main'
+//import { connect } from 'react-redux'
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import AdventureContainer from './containers/AdventureContainer'
+import Adventure from './components/Adventure'
+import About from './components/About'
+import Home from  './components/Home'
+import Links from './components/Links'
+import Contacts from './components/Contacts'
+
+class App extends React.Component {
+
+
+
+  render() {
+
+
+    return (
+      <Router>
+        <div className="App">
+          <Main />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/adventure/:id'component={Adventure} /> 
+            <Route exact path='/adventures' component={AdventureContainer} />
+            <Route exact path='/About' component={About} />
+            <Route exact path='/Contats' component={Contacts} />
+            <Route exact path='/Links' component={Links} />
+                    
+          </Switch>
+         </div> 
+              
+
+      </Router>
+
+    );
+  }
 }
+
+
+
 
 export default App;
