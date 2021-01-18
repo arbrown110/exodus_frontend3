ca33739d78304cb43a338770691c0883aab12174
Angel_Brown <arbrown110@gmail.com>
2021-01-17 22:48:12 -0500

    transfer of about and contact.js.

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="about">
+            <br>
+            </br>
+           This is a little of my experience going to the country of Sierre Leone. Follow me on my journey of how I navigating the country with the people , culture and food.
+
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Contacts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div >
+           For more information Contact me at:
+
+           (886)-543-6978
+        </div>
+    )
+}
+
+export default Contact
