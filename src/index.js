ebe5dcd20b58daa7f3b5e3ef3c45d82e7dc13eb7
Angel_Brown <arbrown110@gmail.com>
2021-01-17 22:40:28 -0500

    app.js.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
+import { combineReducers } from 'redux';
+import adventureReducer from './reducers/adventureReducer';
+
+const rootReducer = combineReducers({advenutres: adventureReducer})
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+      <Router>
+          <App />
+      </Router>
+  </Provider>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+export default rootReducer;
